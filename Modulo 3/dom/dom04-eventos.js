0ae7730ae1123e6280e188c2f40416300dd9ce63
yochan <iwanagayoshihiko@gmail.com>
2026-06-08 08:40:43 -0500

    dom 2

---
+++ Modulo 3/dom/dom04-eventos.js
@@ -0,0 +1,17 @@
+function mostrarAlerta() {
+    alert("Hola desde el evento onclick");
+}
+
+function agregarProducto() {
+    const lista = document.getElementById("lista-productos1");
+    const nuevoProducto = document.createElement("li");
+    nuevoProducto.textContent = "Nuevo Producto";
+    lista.appendChild(nuevoProducto);
+}
+
+document.getElementById("btn3").addEventListener("click", function() {
+    const lista2 = document.getElementById("lista-productos1");
+    const nuevoProducto2 = document.createElement("li");
+    nuevoProducto2.textContent = "Producto desde Listener";
+    lista2.appendChild(nuevoProducto2);
+});
